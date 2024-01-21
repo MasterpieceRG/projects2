d99a9d5be247412df11c49e346b00ac7760fa010
MasterpieceRG <mdrabbigazi2000@gmail.com>
2024-01-21 12:54:44 +0600

    note added

---
+++ ViewModelLiveData/app/src/main/java/com/example/viewmodellivedata/MainActivityViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.viewmodellivedata
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel:ViewModel() {
+    private val counterLiveData = MutableLiveData<Int>()
+    private var counter = 100
+    fun getInitialCounter():MutableLiveData<Int> {
+        counterLiveData.value = counter
+        return counterLiveData
+    }
+    fun getCounter(){
+        counter--
+        counterLiveData.value = counter
+    }
+}
